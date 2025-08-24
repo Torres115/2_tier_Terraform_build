e68a0e94b9c44c1e431ee61b87f9d3b5c372277a
Fernando Torres <fernandotorres1000@gmail.com>
2025-08-24 19:03:01 +0000

    adding

--- vpc.tf
+++ vpc.tf
@@ -2,27 +2,44 @@ provider "aws" {
   region = "us-east-2"
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "Nando_vpc" {
   source                    = "git::https://github.com/Coalfire-CF/terraform-aws-vpc-nfw.git?ref=v3.0.0"
   vpc_name                  = "Nando_vpc"
   cidr                      = "10.1.0.0/16"
-  flow_log_destination_type = "cloud-watch-logs"
 
-  azs = ["us-east-2a", "us-east-2b"]
+  azs = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
 
   subnets = [
-    { tag = "management",  cidr = "10.1.0.0/24", type = "public",  availability_zone = "us-east-2a" },
-    { tag = "application", cidr = "10.1.1.0/24", type = "private", availability_zone = "us-east-2a" },
-    { tag = "backend",     cidr = "10.1.2.0/24", type = "private", availability_zone = "us-east-2b" }
+    {
+      tag               = "Mgt"
+      cidr              = "10.1.0.0/24"
+      type              = "public"
+      availability_zone = "us-east-2a"
+    },
+    {
+      tag               = "APP"
+      cidr              = "10.1.1.0/24"
+      type              = "private"
+      availability_zone = "us-east-2a"
+    },    
+    {
+      tag               = "BKE"
+      cidr              = "10.1.2.0/24"
+      type              = "private"
+      availability_zone = "us-east-2b"
+    }
   ]
-
-  enable_nat_gateway     = false
   single_nat_gateway     = false
-  one_nat_gateway_per_az = false
+  enable_nat_gateway     = true
+  one_nat_gateway_per_az = true
   enable_vpn_gateway     = false
   enable_dns_hostnames   = true
-  deploy_aws_nfw         = false
-}
 
-output "vpc_id"   { value = module.Nando_vpc.vpc_id }
-output "subnets"  { value = module.Nando_vpc.subnets }
+
+  flow_log_destination_type              = "cloud-watch-logs"
+  cloudwatch_log_group_retention_in_days = 30
+}
